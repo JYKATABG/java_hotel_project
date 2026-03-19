e3451dd902acf54176824d8e722496904b217688
Georgi Tsanev <georgi.tsanev975@gmail.com>
2026-03-20 00:29:21 +0200

    feat: add guest model

--- src/models/guests/Guest.java
+++ src/models/guests/Guest.java
@@ -1,5 +1,39 @@
 package models.guests;
 
 public class Guest {
-    
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+
+    public Guest (int id, String firstName, String lastName,
+                    String phone, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public int getId () { return id; }
+    public void setIt (int id) { this.id = id; }
+
+    public String getFirstName () { return firstName; }
+    public void setFirstName (String firstName) { this.firstName = firstName; }
+
+    public String getLastName () { return lastName; }
+    public void setLastName (String lastName) {this.lastName = lastName; }
+
+    public String getPhone () { return phone; }
+    public void setPhone (String phone) { this.phone = phone; }
+
+    public String getEmail () { return email; }
+    public void setEmail (String email) { this.email = email; }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " (" + phone + ")";
+    }
 }
