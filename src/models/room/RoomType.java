2dec3216e75e7d8bbd38cdbf47b8723ef6daeaea
Jykata <jykatabg@gmail.com>
2026-03-20 00:04:30 +0200

    feat: add roomType model

--- src/models/room/RoomType.java
+++ src/models/room/RoomType.java
@@ -1,5 +1,55 @@
 package models.room;
 
 public class RoomType {
-    
+    private int id;
+    private String typeName;
+    private int capacity;
+    private double pricePerNight;
+
+    public RoomType() {
+    };
+
+    RoomType(int id, String typeName, int capacity, double pricePerNight) {
+        this.id = id;
+        this.typeName = typeName;
+        this.capacity = capacity;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public void setPricePerNight(double pricePerNight) {
+        this.pricePerNight = pricePerNight;
+    }
+
+    @Override
+    public String toString() {
+        return typeName;
+    }
 }
