7dc3929ce7c7e8b419cd5267e947e3b16b12ef57
Jykata <jykatabg@gmail.com>
2026-03-20 02:57:17 +0200

    fix: spelling mistake in guest model

--- src/models/guests/Guest.java
+++ src/models/guests/Guest.java
@@ -18,7 +18,7 @@ public class Guest {
     }
 
     public int getId () { return id; }
-    public void setIt (int id) { this.id = id; }
+    public void setId (int id) { this.id = id; }
 
     public String getFirstName () { return firstName; }
     public void setFirstName (String firstName) { this.firstName = firstName; }
